3116310624b562e08457557165ed430837168c24
Benjamin Flügel <bf@vionetworks.de>
2017-10-05 10:47:44 +0200

    Initial commit

---
+++ src/IDatenhaltung.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Created by bf on 05.10.17.
+ */
+public interface IDatenhaltung {
+  //ArrayList<> getLaenderList();
+
+}
---
+++ src/Projekt_Laender_Staedte.java
@@ -0,0 +1,8 @@
+/**
+ * Created by bf on 05.10.17.
+ */
+public class Projekt_Laender_Staedte {
+  public static void main (String[] args) {
+
+  }
+}
