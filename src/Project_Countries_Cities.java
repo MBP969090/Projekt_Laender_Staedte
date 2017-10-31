684d7c54a51881448b837e70da70c2b5eda50689
Peter Ganzschow <peter.ganzschow@gmx.de>
2017-10-31 18:37:30 +0100

    Add Exceptions, change functions

--- src/Project_Countries_Cities.java
+++ src/Project_Countries_Cities.java
@@ -1,3 +1,5 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 /**
  * Created by Benjamin Flügel on 05.10.17.
  * To use the MySQL-Database start MAMP
@@ -5,6 +7,12 @@
  */
 public class Project_Countries_Cities {
   public static void main (String[] args) {
-
+    IBusiness_logic blns = new Business_logic_not_sorted(new SQL_Databasemanagement());
+    try {
+      blns.read_country(1);
+    } catch (Exception e) {
+      System.out.print(e.getStackTrace());
+    }
+    blns.change_country_name("HAMBURG");
   }
 }
