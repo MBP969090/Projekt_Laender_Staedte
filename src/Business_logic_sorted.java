6d2e495a8621985a9b06738447f9d059956fd9e4
Peter <peter.ganzschow@gmx.de>
2017-11-09 12:14:51 +0100

    remove functions which are in abstract class

--- src/Business_logic_sorted.java
+++ src/Business_logic_sorted.java
@@ -3,46 +3,7 @@ import com.sun.javaws.exceptions.InvalidArgumentException;
 /**
  * Created by Peter Ganzschow
  */
-public class Business_logic_sorted implements IBusiness_logic {
-	@Override
-	public void read_country(int id) throws InvalidArgumentException {
-		
-	}
-	
-	@Override
-	public void read_town(int id) throws InvalidArgumentException {
-		
-	}
-	
-	@Override
-	public void change_country_name(String name) throws NullPointerException {
-		
-	}
-	
-	@Override
-	public void change_town_name(String name) throws NullPointerException {
-		
-	}
-	
-	@Override
-	public void delete_country() throws NullPointerException {
-		
-	}
-	
-	@Override
-	public void delete_town() throws NullPointerException {
-		
-	}
-	
-	@Override
-	public void add_country(String name) {
-		
-	}
-	
-	@Override
-	public void add_town(String name) throws NullPointerException {
-		
-	}
+public class Business_logic_sorted extends Abstract_business_logic {
 	
 	@Override
 	public Country[] get_countries() {
